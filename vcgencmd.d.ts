af671f055145f0820e10e2cad072e0ac7a8ff537
Paul Loyd <pavelko95@gmail.com>
2014-03-31 20:01:26 +0400

    add TS definition

---
+++ vcgencmd.d.ts
@@ -0,0 +1,35 @@
+declare module "vcgencmd" {
+    export function measureClock(clock: 'arm'): number;
+    export function measureClock(clock: 'core'): number;
+    export function measureClock(clock: 'h264'): number;
+    export function measureClock(clock: 'isp'): number;
+    export function measureClock(clock: 'v3d'): number;
+    export function measureClock(clock: 'uart'): number;
+    export function measureClock(clock: 'pwm'): number;
+    export function measureClock(clock: 'emmc'): number;
+    export function measureClock(clock: 'pixel'): number;
+    export function measureClock(clock: 'vec'): number;
+    export function measureClock(clock: 'hdmi'): number;
+    export function measureClock(clock: 'dpi'): number;
+    export function measureClock(clock: string): number;
+
+    export function measureTemp(): number;
+
+    export function codecEnabled(codec: 'H264'): boolean;
+    export function codecEnabled(codec: 'MPG2'): boolean;
+    export function codecEnabled(codec: 'WVC1'): boolean;
+    export function codecEnabled(codec: 'MPG4'): boolean;
+    export function codecEnabled(codec: 'MJPG'): boolean;
+    export function codecEnabled(codec: 'WMV9'): boolean;
+    export function codecEnabled(codec: string): boolean;
+
+    export function getConfig(config: 'int'): {[key: string]: number};
+    export function getConfig(config: 'str'): {[key: string]: string};
+    export function getConfig(config: string): any;
+
+    export function getCamera(): {detected: boolean; supported: boolean};
+
+    export function getMem(mem: 'arm'): number;
+    export function getMem(mem: 'gpu'): number;
+    export function getMem(mem: string): number;
+}
